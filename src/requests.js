bf181a2c2c08033d9dda449d53d3889dd1de1dcf
hasan-furkan <hsnfrkn32@gmail.com>
2021-09-09 04:10:13 +0300

    update

--- src/requests.js
+++ src/requests.js
@@ -1,14 +1,14 @@
 const API_KEY = "8830c70f69884cd02570d0f278a1bb95";
 
 const requests = {
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}$with_networks=213`,
-    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genre=28`,
-    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genre=35`,
-    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genre=27`,
-    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genre=10749`,
-    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genre=99`,
-}
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}`,
+  fetchActionMovies: `/discover/tv?api_key=${API_KEY}`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}`,
+  fetchHorrorMovies: `/discover/tv?api_key=${API_KEY}`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}`,
+};
 
 export default requests;
